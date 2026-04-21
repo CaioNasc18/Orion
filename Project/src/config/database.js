a29a4b482935e49b9960e2e583c52e51c3619539
CaioNasc18 <caio.nascimento.1702@gmail.com>
2026-04-21 16:15:25 +0100

    commit base projeto 3

--- Project/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('nome_da_bd', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-module.exports = sequelize;
