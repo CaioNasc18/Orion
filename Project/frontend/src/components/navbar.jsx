809158141347364a9d0b90f252005ad1a1d304d4
Neru <angelasrferreira@gmail.com>
2026-05-05 16:52:42 +0100

    Navbar done!

--- Project/frontend/src/components/navbar.jsx
+++ Project/frontend/src/components/navbar.jsx
@@ -1,7 +1,62 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light px-4">
-      <span className="navbar-brand">CyberBox</span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+      <div className="container">
+        
+        {/* Logo */}
+        <a className="navbar-brand fw-bold" href="#">
+          CyberBox
+        </a>
+
+        {/* Botão mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">Services</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">NIS2</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">Sectors</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">Methodology</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">News</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacts</a>
+            </li>
+
+            <button className="btn btn-light btn-lg">
+              Área de Cliente
+            </button>
+
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
