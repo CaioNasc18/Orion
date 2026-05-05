d75f3e7e451d8c38d969312bcbcc728ea647d277
Robinson <adrianacapela09@gmail.com>
2026-05-05 17:16:22 +0100

    error fix

--- Project/frontend/src/pages/news.jsx
+++ Project/frontend/src/pages/news.jsx
@@ -1,7 +1,4 @@
  import Navbar from "../components/Navbar";
-import Hero from "../components/sections/Hero";
-import Context from "../components/sections/Context";
-import About from "../components/sections/About";
 import Footer from "../components/Footer";
  
 
