ce43479e09c7ec857864b000e5b9e8b8de7f3b0c
Robinson <adrianacapela09@gmail.com>
2026-05-25 16:13:15 +0100

    BDOnlineNeon

--- Project/backend/src/config/database.js
+++ Project/backend/src/config/database.js
@@ -6,7 +6,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432, // Garante que usa a porta do .env
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Necessário para evitar erros de certificado autoassinado no Neon
+      },
+    },
   }
 );
 
